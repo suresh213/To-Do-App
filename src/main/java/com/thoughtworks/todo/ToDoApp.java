de07c4777e7f803fc6270a483248baba7415fcc0
Suresh <suresh@Sureshs-MacBook-Pro.local>
2022-05-11 20:42:58 +0530

    Initial setup

---
+++ src/main/java/com/thoughtworks/todo/ToDoApp.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SpringApplication.run(ToDoApp.class,args);
+
+	}
+
+}
